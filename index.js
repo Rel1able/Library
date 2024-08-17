fbc62e4b30cbfce106085024ad6553abf7c6b04e
Yaroslav <yaroslav.shebovovkk@gmail.com>
2024-08-17 08:21:04 +0200

    added book to library

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function Book() {
 
 }
 
+
+
 function addBookToLibrary() {
 
 }
@@ -12,23 +14,26 @@ function showForm() {
     document.getElementById("add-book").style.display = "block";
 }
 
-
-
-
 let addButton = document.querySelector(".btn-add");
-
 addButton.addEventListener("click", () => {
     let name = document.getElementById("name").value;
-    console.log(name);
     let author = document.getElementById("author").value;
-    console.log(author);
     let pages = document.getElementById("pages").value;
-    console.log(pages);
     myLibrary.push({
         name: name,
         author: author,
         pages: pages
     })
     document.getElementById("add-book").style.display = "none";
+    document.getElementById("name").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+
     console.log(myLibrary);
 })
+
+
+
+
+
+
